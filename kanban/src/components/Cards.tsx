87612474d36d69c095e7c1a3d2fad039aebcc2ee
Moksh-Mutreja <mokshmutreja23@gmail.com>
2025-08-20 02:40:46 +0530

    improve UI

--- kanban/src/components/Cards.tsx
+++ kanban/src/components/Cards.tsx
@@ -1,21 +1,20 @@
+import type { Status } from "../types";
 
-export const Card = ({
-  title,
-  id,
-  points,
-}: {
+interface CardProps {
   title: string;
   id: string;
-  points?: number;
-}) => {
+  points: number;
+  status?: Status; 
+}
+
+export const Card = ({ title, id, points }: CardProps) => {
   return (
-    <div className="border rounded-lg p-3 m-2 bg-neutral-950 text-white hover:shadow-xl/30 ">
-      <div className="text-xl py-2 font-medium">{title}</div>
-      <div className="flex gap-5 justify-between py-3 text-zinc-300">
+    <div className="border rounded-lg p-3 m-2  bg-neutral-950 text-white w-auto hover:shadow-xl/30 ">
+      <div className="font-mono py-2 text-lg">{title}</div>
+      <div className="flex gap-5 justify-between py-3 text-zinc-300 text-sm">
         <div>{id}</div>
         <div>{points}</div>
       </div>
     </div>
   );
 };
-
