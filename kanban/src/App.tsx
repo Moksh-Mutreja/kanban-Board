9dfd6e0f52eac3c5b394eacf2995ac023bc61691
Moksh-Mutreja <mokshmutreja23@gmail.com>
2025-08-20 16:14:47 +0530

    add drag and drop

--- kanban/src/App.tsx
+++ kanban/src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { Card } from "./components/Cards";
-import type { Status, Priority,} from "./types";
-import { statuses, priorities ,tasks} from "./types";
-export default function App() {
+import type { Status, Priority } from "./types";
+import { statuses, priorities, tasks as initialTasks } from "./types";
 
-  const Columns = statuses.map((status) => {
+export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const handleDrop = (e: React.DragEvent, newStatus: Status) => {
+    e.preventDefault();
+    const taskId = e.dataTransfer.getData("taskId");
+
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const columns = statuses.map((status) => {
     const tasksColumn = tasks.filter((task) => task.status === status);
     return {
       status,
@@ -12,28 +30,32 @@ export default function App() {
   });
 
   return (
-    <>
-      <div className="flex divide-x p-2 m-2 gap-2">
-        {Columns.map((column) => (
-          <div key={column.status} className="flex-1">
-            <h1 className="text-3xl p-3 m-3 text-center text-cyan-900">
-              {column.status === "pending" && "Pending Tasks"}
-              {column.status === "in-progress" && "In-Progress Tasks"}
-              {column.status === "done" && "Done Tasks"}
-            </h1>
-     
-            {column.tasks.map((task) => (
-              <Card
-                key={task.id}
-                title={task.title}
-                id={task.id}
-                points={task.points}
-                priority={task.priority as Priority}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex divide-x p-2 m-2 gap-2 h-screen">
+      {columns.map((column) => (
+        <div
+          key={column.status}
+          className="flex-1 flex flex-col h-full bg-neutral-100 rounded-md "
+          onDrop={(e) => handleDrop(e, column.status)}
+          onDragOver={handleDragOver}
+        >
+          <h1 className="text-3xl p-3 m-3 text-center text-cyan-900">
+            {column.status === "pending" && "Pending Tasks"}
+            {column.status === "in-progress" && "In-Progress Tasks"}
+            {column.status === "done" && "Done Tasks"}
+          </h1>
+
+          {column.tasks.map((task) => (
+            <Card
+              key={task.id}
+              title={task.title}
+              id={task.id}
+              points={task.points}
+              priority={task.priority}
+              status={task.status}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
